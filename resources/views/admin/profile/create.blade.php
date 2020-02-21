63f2a61c98c90afd18d3ade0fb35ac6ff8c2f141
”kodama” <kodamaperc@gmail.com>
2020-02-21 03:35:21 +0000

    PHP/Laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,26 @@
         <mata http-equiv="X-UA-Compatible" content="IE=edge">
         <mata name="viewport" content="width=device-width, initial-scale-1">
             
-            <title>add課題表記</title>
+            <title>プロフィール</title>
     </head>
     <body>
-        <h1>add課題作成</h1>
+        <h1>プロフィール</h1>
+                {{-- layouts/profile.blade.phpを読み込む --}}
+        @extends('layouts.profile')
+
+
+        {{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+        @section('title', 'Myプロフィール')
+
+        {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>Myプロフィール</h2>
+                    </div>
+                </div>
+    </div>
+@endsection
     </body>
 </html>  
