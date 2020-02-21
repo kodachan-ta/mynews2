58e64116a2e5236be67c0ad3e1b577703145d605
”kodama” <kodamaperc@gmail.com>
2020-02-21 14:44:00 +0000

    PHP/Laravel14

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -55,6 +55,9 @@
                                     <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                                 </div>
                             </div>
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        　</form>
                     </div>
                 </div>
             </div>
