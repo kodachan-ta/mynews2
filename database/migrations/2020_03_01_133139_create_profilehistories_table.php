83c66bf1fcdeb2c3881d45ffc90fefb3fe26fb94
”kodama” <kodamaperc@gmail.com>
2020-03-01 14:17:43 +0000

    PHP\Laravel17

--- database/migrations/2020_03_01_133139_create_profilehistories_table.php
+++ database/migrations/2020_03_01_133139_create_profilehistories_table.php
@@ -13,7 +13,7 @@ class CreateProfilehistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('profilehistories', function (Blueprint $table) {
+        Schema::create('profile_histories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('profile_id');
             $table->string('edited_at');
